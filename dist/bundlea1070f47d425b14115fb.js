d2f4c660db5ebe4370d8fc56f4ded6ce81a6aeb0
Ryan Valera <valeraryan@gmail.com>
2023-09-17 20:02:54 -0700

    Created the default template for the main template.html and index.css page.

--- dist/bundlea1070f47d425b14115fb.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-console.log('Hello');
-/******/ })()
-;
-//# sourceMappingURL=bundlea1070f47d425b14115fb.js.map
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-console.log('Hello');
+import './styles/index.css';
